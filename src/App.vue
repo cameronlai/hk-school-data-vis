d1e224f37bdcb5a3303e7a92bb4c1895dd803a11
cameronlai <cameronlai@users.noreply.github.com>
2019-03-23 17:05:35 +0800

    Complete combo box selection and data change. Still need to work on selection changes.

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,11 @@
           <v-toolbar-side-icon></v-toolbar-side-icon>
           <v-toolbar-title>Hong Kong School Finder</v-toolbar-title>
           <v-spacer></v-spacer>
-          <!--
-          <v-btn icon>
-            <v-icon>search</v-icon>
-          </v-btn>
-          -->
         </v-toolbar>
         <MySelection
           v-for="school_selection in school_selection_data"
           v-bind:prop_selection="school_selection"
+          v-on:change-selection="changeCriteria"
         />
         <MyTable 
           v-bind:prop_school_table_headers="school_table_header"
@@ -36,11 +32,14 @@ export default {
   el: "#app",
   data() {
     return {
+      // Actual data
       school_header: [],
-      school_display_column: ['D', 'Z'],
-      school_selection_columns: ["B", "P", "R", "T", "V", "X", "AF"],
-      school_selection_data: [],
       school_data: [],
+      // Selection data
+      school_display_column: ['D', 'Z'],
+      school_selection_columns: ["B"],//, "P", "R", "T", "V", "X", "AF"],
+      school_selection_data: [],
+      // Table data
       school_table_header: [],
       school_table_data: [
           {B: 'asdfasdfasdf', D: 'asdfsdfsadfds'},
@@ -48,23 +47,25 @@ export default {
     };
   },
   mounted() {
-    //axios.get("/data3.json").then(response => {
-    axios.get("/real_data.json").then(response => {
+    axios.get("/data3.json").then(response => {
+    //axios.get("/real_data.json").then(response => {
       console.log("Get data");
       this.school_header = response.data.shift();
       console.log(this.school_header);
       this.school_data = response.data;
       console.log(this.school_data[0]);
-
-      this.getSelectionItems();
-      //this.changeCriteria();
+      
+      this.school_table_data = this.school_data; // Get table data
+      this.getTableHeader();
+      this.getSelectionItems(); // Get selection items based on table data
     });
   },
   methods: {
     loadMore: function() {
       console.log("hello " + event.messages);
     },
-    getSelectionItems: function() {
+    getTableHeader: function() {
+      console.log("Get Table Header");
       for (var i = 0; i< this.school_display_column.length; i++) {
         var col_name = this.school_display_column[i];
         var header = this.school_header[col_name];
@@ -73,23 +74,43 @@ export default {
         header_obj['value'] = col_name;
         this.school_table_header.push(header_obj);
       }
+    },
+    getSelectionItems: function() {
+      console.log("Get Selection Data");
       for (var i = 0; i < this.school_selection_columns.length; i++) {
         var col_name = this.school_selection_columns[i];
         var header = this.school_header[col_name];
         var item = {};
+        console.log(header);
         item["label"] = header;
         item["items"] = Array.from(
+          // Note that this is based on school table data to provide dynamic filters
           new Set(this.school_data.map(item => item[col_name]))
         );
         this.school_selection_data.push(item);
       }
     },
+    filterFunc: function(item) {
+      console.log("Filter");
+      for (var i = 0; i < this.school_selection_columns.length; i++) {      
+          var col_name = this.school_selection_columns[i];
+          var selected_item = this.school_selection_data[i].item;          
+          console.log(col_name);
+          console.log(selected_item);
+          if (selected_item == undefined) {
+            continue;
+          }
+          if (item[col_name] == undefined || item[col_name] != selected_item) {
+            return false;
+          }          
+        }
+        return true;
+    },
     changeCriteria: function() {
-      console.log(this.selected_school_type);
-      this.filtered_school_data = this.school_data.filter(
-        school => school.B == this.selected_school_type
-      );
-      console.log(this.filtered_school_data);
+      console.log("Change Criteria");
+      console.log(this.school_selection_data);
+      this.school_table_data = this.school_data.filter(this.filterFunc);      
+      //this.getSelectionItems();      
     }
   },
   components: {
--- src/components/MySelection.vue
+++ src/components/MySelection.vue
@@ -5,6 +5,8 @@
         :items="prop_selection.items"
         :label="prop_selection.label"
         v-on:change="$emit('change-selection')"
+        clearable
+        clear-icon="clear"
       >     
       </v-combobox>
     </v-flex>
@@ -13,13 +15,9 @@
 <script>
 export default {
   name: "MySelection",
-  components: {
-  },
   props: ['prop_selection'],
   data() {
-    return {
-      //selected_item: "Kindergartens",
-      //selected_items: ["Aided Primary Schools", "Kindergartens", "Direct Subsidy Scheme Primary Schools", "Aided Special Schools"]
+    return {            
     };
   }
 };
